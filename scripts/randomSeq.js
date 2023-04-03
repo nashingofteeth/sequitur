b6c4f167b88deba7aa5308b1914be039bceef592
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-04-02 23:44:33 -0700

    Create random sequencer

--- scripts/randomSeq.js
+++ scripts/randomSeq.js
@@ -3,6 +3,16 @@ const { exec } = require("child_process");
 
 console.clear();
 
+var outputFrameRate = 60,
+    frameRates = [], // divisors of 240
+    targetDuration = 60; // seconds
+
+// get divisors of frameRate
+for (let i=0; i<outputFrameRate; i++) {
+    if ( (60 % i) == 0) frameRates.push(i);
+}
+frameRates.push(outputFrameRate);
+
 // exec('mkdir temp temp/frames exports');
 // exec('ffmpeg -i input/video.mov -vf scale=-1:240 -qscale:v 2 temp/frames/%d.jpg', (error, stdout, stderr) => {
 //     console.log(error, stdout, stderr);
@@ -11,5 +21,28 @@ console.clear();
 
 const dir = 'temp/frames/';
 fs.readdir(dir, (err, files) => {
-    console.log(files.length);
+    sequence(files.length);
 });
+
+function sequence(numOfFrames) {
+    var frames = [],
+        selectedFrame = 0,
+        frameDuration = 0,
+        totalDuration = 0,
+        loops = targetDuration * frameRates[frameRates.length-1];
+
+    for (let i = 0; i < loops; i++) {
+        frameDuration = 1/frameRates[Math.floor(Math.random()*frameRates.length)];
+        selectedFrame = Math.floor(Math.random()*numOfFrames);
+
+        if (totalDuration + frameDuration > targetDuration) {
+            i = loops-1;
+            frameDuration = targetDuration - totalDuration;
+        }
+        totalDuration += frameDuration;
+        
+        frames.push([selectedFrame, frameDuration]);
+    }
+
+    console.log(frames);
+}
