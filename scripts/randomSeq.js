a025a2160c1388643125c4b7d734c184c32b7802
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-04-02 20:20:03 -0700

    Count files

--- scripts/randomSeq.js
+++ scripts/randomSeq.js
@@ -3,8 +3,13 @@ const { exec } = require("child_process");
 
 console.clear();
 
-exec('mkdir temp temp/frames exports');
-exec('ffmpeg -i input/video.mov -vf scale=-1:240 -qscale:v 2 temp/frames/%d.jpg', (error, stdout, stderr) => {
-    console.log(error, stdout, stderr);
-    console.log('DONE\n');
-}); 
+// exec('mkdir temp temp/frames exports');
+// exec('ffmpeg -i input/video.mov -vf scale=-1:240 -qscale:v 2 temp/frames/%d.jpg', (error, stdout, stderr) => {
+//     console.log(error, stdout, stderr);
+//     console.log('DONE\n');
+// }); 
+
+const dir = 'temp/frames/';
+fs.readdir(dir, (err, files) => {
+    console.log(files.length);
+});
