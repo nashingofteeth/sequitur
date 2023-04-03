417f334115460671a97bd677187b4303c6b370d1
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-04-02 20:14:33 -0700

    Initializing media and folders

---
+++ scripts/randomSeq.js
@@ -0,0 +1,10 @@
+const fs = require("mz/fs");
+const { exec } = require("child_process");
+
+console.clear();
+
+exec('mkdir temp temp/frames exports');
+exec('ffmpeg -i input/video.mov -vf scale=-1:240 -qscale:v 2 temp/frames/%d.jpg', (error, stdout, stderr) => {
+    console.log(error, stdout, stderr);
+    console.log('DONE\n');
+}); 
