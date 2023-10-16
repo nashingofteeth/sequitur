128769a13b6c65c4d030ef946ab74e2f57e02a99
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-10-15 22:21:16 -0700

    remove unnecessary promises

--- scripts/sequitur.js
+++ scripts/sequitur.js
@@ -1,6 +1,6 @@
 const fs = require("mz/fs"),
       path = require('path'),
-      { exec } = require("child_process"),
+      { execSync } = require("child_process"),
       compareImages = require('resemblejs/compareImages'),
       wav = require('node-wav');
 
@@ -27,18 +27,20 @@ const videoFile = ( args['vid'] && fs.existsSync(args['vid']) ) ? args['vid'] : 
       minOffset = parseFloat(args['min']),
       initialize = args['init'];
 
-async function init() {
-    // require some args
-    if ( !resolution || !framerate || !videoFile || !audioFile ) {
-        console.log('missing/invalid args!');
-        return;
-    }
+// require some args
+if ( !resolution || !framerate || !videoFile || !audioFile ) {
+    console.log('missing/invalid args!');
+    return;
+}
 
-    // make or remove temp folder
-    const dir = 'temp';
-    if (initialize && fs.existsSync(dir)) fs.rmSync(dir, { recursive: true, force: true });
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+// make or remove temp folder
+const dir = 'temp';
+if (initialize && fs.existsSync(dir))
+    fs.rmSync(dir, { recursive: true, force: true });
+if (!fs.existsSync(dir))
+    fs.mkdirSync(dir);
 
+(async() => {
     // load data
     const numOfFrames = await loadFrames(),
           waveform = await readWaveform(),
@@ -46,85 +48,50 @@ async function init() {
 
     // sequence and encode
     const seq = sequence(numOfFrames, waveform, framerate, maxLevel, minOffset);
-    const written = await write('temp/seq.txt', seq);
-    await encode(written, resolution, framerate, audioFile, preview);
-}
+    await encode(seq, resolution, framerate, audioFile, preview);
+})()
 
-
-function write(file, data) {
-    return new Promise(function(resolve, reject){
-        fs.writeFile(file, data, function (err) {
-            if (err) reject(error);
-            console.log(file + ' written');
-            resolve(true);
-        });
-    })
-}
-function read(file) {
-    return new Promise(function(resolve, reject) {
-        fs.readFile(file, function(err, data) {
-            if (err) resolve(false);
-            else {
-                console.log(file + ' read');
-                resolve(data);
-            }
-        });   
-    });
-}
 
 async function loadFrames() {
-    const dir = 'temp';
-    let numOfFrames = await countFrames();
-
-    
+    let numOfFrames = countFrames();
 
     if (!numOfFrames) {
-        await extractFrames(videoFile, resolution);
-        numOfFrames = await countFrames();
+        extractFrames(videoFile, resolution);
+        numOfFrames = countFrames();
     }
 
     return numOfFrames;
 }
 
 function countFrames() {
-    return new Promise(function(resolve, reject) {
-        fs.readdir('temp/frames/', (err, files) => {
-            if (!files || files.length < 2 || err) resolve(false);
-            else {
-                const frames = files.filter(el => path.extname(el) === '.jpg');
-                resolve(frames.length);
-            }
-        });
-    });
+    const dir = 'temp/frames/';
+    if (fs.existsSync(dir)) {
+        files = fs.readdirSync(dir);
+        if (files.length < 2) return false;
+        else {
+            const frames = files.filter(el => path.extname(el) === '.jpg');
+            return frames.length;
+        }
+    }
+    else return false;
 }
 
 function extractFrames(file, res) {
     const dir = 'temp/frames/';
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
+    if (!fs.existsSync(dir))
+        fs.mkdirSync(dir)
 
-    const extractCmd = "ffmpeg -i " + file.replace(' ','\\ ') + " -vf scale=-1:" + res + " -qscale:v 2 temp/frames/%d.jpg -y";
-
-    console.log('extracting frames...');
-    return new Promise(function(resolve, reject) {
-        exec(extractCmd, (err, stdout, stderr) => {
-            if (err) reject(err);
-            // console.log(error, stdout, stderr);
-            console.log('frames extracted');
-            resolve(true);
-        });
-    });
+    execSync("ffmpeg -i " + file.replace(' ','\\ ') + " -vf scale=-1:" + res + " -qscale:v 2 temp/frames/%d.jpg -y");
 }
 
 async function readWaveform() {
-    const file = 'temp/wave.json',
-          data = await read(file);
+    const file = 'temp/wave.json';
 
-    if (data) waveform = JSON.parse(data);
+    if (fs.existsSync(file)) 
+        waveform = JSON.parse(fs.readFileSync(file));
     else {
         waveform = await sampleWaveform(audioFile, framerate);
-        await write(file, JSON.stringify(waveform));
+        fs.writeFileSync(file, JSON.stringify(waveform));
     }
 
     return waveform;
@@ -155,18 +122,17 @@ function sampleWaveform(file, fps) {
         conformed.push( (resampled[s] * (1 / max)).toFixed(4) );
     }
 
-    console.log('waveform sampled');
     return conformed;
 }
 
 async function loadDiffs(numOfFrames) {
     const file = 'temp/diffs.json';
-    let data = await read(file),
-    diffs = JSON.parse(data);
 
-    if (!data || Object.keys(diffs).length != numOfFrames) {
+    if (fs.existsSync(file))
+        diffs = JSON.parse(fs.readFileSync(file));
+    else {
         diffs = await compareFrames(numOfFrames);
-        await write(file, JSON.stringify(diffs));
+        fs.writeFileSync(file, JSON.stringify(diffs));
     }
 
     return diffs;
@@ -255,17 +221,15 @@ function sequence(numOfFrames, waveform, fps, max, min) {
                "duration " + frames[f][1] + "\n";
     }
 
-    console.log('sequenced');
     return seq;
 }
 
-function encode(written, res, fps, aud, pre) {
-    if (!written) process.exit();
+function encode(seq, res, fps, aud, pre) {
+    fs.writeFileSync('temp/seq.txt', seq);
 
     let dir = 'exports';
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir))
         fs.mkdirSync(dir);
-    }
 
     const preview = "ffmpeg -f concat -i temp/seq.txt -vf scale=-1:" + res + " -vcodec libx264 -crf 30 -pix_fmt yuv420p -fps_mode vfr -r " + fps + " exports/sequitur_" + Date.now() + ".mp4 -i " + aud.replace(' ','\\ '),
           full = "ffmpeg -f concat -i temp/seq.txt -vf scale=-1:" + res + " -c:v prores_ks -profile:v 2 -c:a pcm_s16le -fps_mode vfr -r " + fps + " exports/sequitur_" + Date.now() + ".mov -i " + aud.replace(' ','\\ ');
@@ -273,14 +237,6 @@ function encode(written, res, fps, aud, pre) {
     const encodeCmd = pre ? preview : full;
 
     console.log('encoding...');
-    return new Promise(function(resolve, reject){
-        exec(encodeCmd, (err, stdout, stderr) => {
-            // console.log(stdout, stderr);
-            if (err) reject(err);
-            console.log('encoded');
-            resolve(true);
-        });
-    })
+    execSync(encodeCmd);
+    console.log('encoded');
 }
-
-init();
