d9a0a05df99c1efed282d1997ac807e31ba351ff
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-10-17 21:50:05 -0700

    create exporting module

--- scripts/sequitur.js
+++ scripts/sequitur.js
@@ -1,12 +1,13 @@
 const fs = require("mz/fs"),
-      { execSync } = require("child_process"),
-      ef = require("./extractFrames"),
-      cf = require('./compareFrames'),
-      sa = require('./sampleAudio'),
-      skip = require('./skipSequencer');
+      extractFrames = require("./extractFrames"),
+      compareFrames = require('./compareFrames'),
+      sampleAudio = require('./sampleAudio'),
+      exportSequence = require('./exportSequence'),
+      skipSequencer = require('./skipSequencer');
 
 console.clear();
 
+// get args
 const validArgs = ['vid', 'aud', 'res', 'fps', 'pre', 'max', 'min', 'init'];
 let args = {}
 for ( a in validArgs) {
@@ -19,8 +20,8 @@ for ( a in validArgs) {
     else args[validArgs[a]] = false;
 }
 
-const videoFile = ( args['vid'] && fs.existsSync(args['vid']) ) ? args['vid'] : false,
-      audioFile = ( args['aud'] && fs.existsSync(args['aud']) ) ? args['aud'] : false,
+const video = ( args['vid'] && fs.existsSync(args['vid']) ) ? args['vid'] : false,
+      audio = ( args['aud'] && fs.existsSync(args['aud']) ) ? args['aud'] : false,
       resolution = parseInt(args['res']),
       framerate = args['fps'] ? parseInt(args['fps']) : 24,
       preview = args['pre'],
@@ -29,7 +30,7 @@ const videoFile = ( args['vid'] && fs.existsSync(args['vid']) ) ? args['vid'] : 
       initialize = args['init'];
 
 // require some args
-if ( !resolution || !videoFile || !audioFile ) {
+if ( !resolution || !video || !audio ) {
     console.log('missing/invalid args!');
     return;
 }
@@ -43,33 +44,11 @@ if (!fs.existsSync(dir))
 
 (async() => {
     // load data
-    const wave = await sa.wave(audioFile, framerate),
-          frames = await ef.frames(videoFile, resolution),
-          diffs = await cf.diffs(frames);
+    const wave = await sampleAudio.wave(audio, framerate),
+          frames = await extractFrames.frames(video, resolution),
+          diffs = await compareFrames.diffs(frames);
 
     // sequence and encode
-    const seq = skip.sequence(frames, wave, framerate, maxLevel, minOffset);
-    encode(seq, resolution, framerate, audioFile, preview);
+    const sequence = skipSequencer.sequence(frames, wave, framerate, maxLevel, minOffset);
+    exportSequence.concat(sequence, resolution, framerate, audio, preview);
 })()
-
-function encode(seq, res, fps, aud, pre) {
-    let seqStr = '';
-    for (f in seq) {
-        seqStr += "file 'frames/" + (seq[f][0]+1) + ".jpg'\n" +
-                  "duration " + seq[f][1] + "\n";
-    }
-    fs.writeFileSync('temp/seq.txt', seqStr);
-
-    let dir = 'exports';
-    if (!fs.existsSync(dir))
-        fs.mkdirSync(dir);
-
-    const preview = "ffmpeg -f concat -i temp/seq.txt -vf scale=-1:" + res + " -vcodec libx264 -crf 30 -pix_fmt yuv420p -fps_mode vfr -r " + fps + " exports/sequitur_" + Date.now() + ".mp4 -i " + aud.replace(' ','\\ '),
-          full = "ffmpeg -f concat -i temp/seq.txt -vf scale=-1:" + res + " -c:v prores_ks -profile:v 2 -c:a pcm_s16le -fps_mode vfr -r " + fps + " exports/sequitur_" + Date.now() + ".mov -i " + aud.replace(' ','\\ ');
-
-    const encodeCmd = pre ? preview : full;
-
-    console.log('encoding...');
-    execSync(encodeCmd);
-    console.log('encoded');
-}
