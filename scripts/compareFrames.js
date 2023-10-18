7661d3bf48a89d81841204473c02c4d455e7d0fb
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-10-18 10:35:31 -0700

    rename numOfFrames

--- scripts/compareFrames.js
+++ scripts/compareFrames.js
@@ -1,40 +1,40 @@
 const fs = require("mz/fs"),
 	  compareImages = require('resemblejs/compareImages');
 
-exports.diffs = async function (numOfFrames) {
+exports.diffs = async function (frameCount) {
     const file = 'temp/diffs.json';
 
     if (fs.existsSync(file))
         diffs = JSON.parse(fs.readFileSync(file));
     else {
-        diffs = await compareFrames(numOfFrames);
+        diffs = await compareFrames(frameCount);
         fs.writeFileSync(file, JSON.stringify(diffs));
     }
 
     return diffs;
 }
 
-async function compareFrames(numOfFrames) {
+async function compareFrames(frameCount) {
     let diffs = {},
         timecode = [],
         threshold = 100;
 
-    for (a = 1; a <= numOfFrames; a++) {
+    for (a = 1; a <= frameCount; a++) {
         timecode.push(Date.now());
         processingTime = timecode[timecode.length-1]-timecode[timecode.length-2];
-        diffsPerSec = (numOfFrames / (processingTime / 1000));
-        diffsLeft = (numOfFrames - a) * numOfFrames;
+        diffsPerSec = (frameCount / (processingTime / 1000));
+        diffsLeft = (frameCount - a) * frameCount;
         secsLeft = Math.round(diffsLeft / diffsPerSec);
         timeLeft = secsLeft > 60 ? Math.round(secsLeft / 60) + 'm' : secsLeft + 's';
-        progress = Math.round((a/numOfFrames)*100);
+        progress = Math.round((a/frameCount)*100);
 
-        message = (a < 2 || a > numOfFrames-1) ? 'comparing frames...' : 'comparing frames - ' + progress + '%, ' + timeLeft + ' left @ ' + Math.round(diffsPerSec) + '/s';
+        message = (a < 2 || a > frameCount-1) ? 'comparing frames...' : 'comparing frames - ' + progress + '%, ' + timeLeft + ' left @ ' + Math.round(diffsPerSec) + '/s';
         console.clear();
         console.log(message);
         
         diffs[a] = {};
         max = 0;
-        for (b = 1; b <= numOfFrames; b++) {
+        for (b = 1; b <= frameCount; b++) {
             if ( b == a ) diff = 0;
 			else if ( b == a-1 ) diff = diffs[a-1][b+1];
             else diff = await getDiff(a, b, threshold);
