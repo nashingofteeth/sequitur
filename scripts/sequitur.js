bd617a3e8c71dddea7b5d78bd5d96cd5d6f4c3bd
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-10-22 23:02:08 -0700

    remove unnecessary async

--- scripts/sequitur.js
+++ scripts/sequitur.js
@@ -1,21 +1,18 @@
-const fs = require("mz/fs"),
-      extractFrames = require("./extractFrames"),
-      compareFrames = require('./compareFrames'),
-      sampleAudio = require('./sampleAudio'),
-      exportSequence = require('./exportSequence'),
-      skipSequencer = require('./skipSequencer');
+const fs = require("mz/fs");
 
 console.clear();
 
 // get args
-var args = require('minimist')(process.argv.slice(2));
+var args = require('minimist')(process.argv.slice(2)),
+    video = ( args['v'] && fs.existsSync(args['v']) ) ? args['v'] : false,
+    audio = ( args['a'] && fs.existsSync(args['a']) ) ? args['a'] : false,
+    framerate = args['r'] ? parseInt(args['r']) : 24,
+    size = args['s'] ? parseInt(args['s']) : 240,
+    preview = args['p'],
+    initialize = args['i'];
 
-const video = ( args['v'] && fs.existsSync(args['v']) ) ? args['v'] : false,
-      audio = ( args['a'] && fs.existsSync(args['a']) ) ? args['a'] : false,
-      framerate = args['fps'] ? parseInt(args['r']) : 24,
-      size = args['s'] ? parseInt(args['s']) : 240,
-      preview = args['p'],
-      initialize = args['i'];
+exports.framerate = framerate;
+exports.args = args;
 
 // require some args
 if ( !video || !audio ) {
@@ -30,13 +27,11 @@ if (initialize && fs.existsSync(dir))
 if (!fs.existsSync(dir))
     fs.mkdirSync(dir);
 
-(async() => {
-    // load data
-    const frames = await extractFrames.frames(video, size),
-          diffs = await compareFrames.diffs(frames),
-          wave = await sampleAudio.wave(audio, framerate);
-
-    // sequence and encode
-    const sequence = skipSequencer.sequence(frames, wave, framerate, args);
-    exportSequence.concat(sequence, size, framerate, audio, preview);
-})()
+// load data
+exports.frameCount = function () { return require("./extractFrames").frames(video, size); }
+exports.diffs = function () { 
+        const frameCount = require("./extractFrames").frames(video, size);
+        return require('./compareFrames').diffs(frameCount);
+}
+exports.wave = function () { return require('./sampleAudio').wave(audio, framerate); }
+exports.export = function (sequence) { require('./exportSequence').concat(sequence, size, framerate, audio, preview); }
