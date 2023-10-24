a11cb86148fc2d339583c26fe11f696bd487c120
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-10-23 22:10:29 -0700

    tweaks to main file

--- src/sequitur.js
+++ src/sequitur.js
@@ -1,18 +1,11 @@
-const fs = require("mz/fs");
-
-console.clear();
-
-// get args
-var args = require('minimist')(process.argv.slice(2)),
-    video = ( args['v'] && fs.existsSync(args['v']) ) ? args['v'] : false,
-    audio = ( args['a'] && fs.existsSync(args['a']) ) ? args['a'] : false,
-    framerate = args['r'] ? parseInt(args['r']) : 24,
-    size = args['s'] ? parseInt(args['s']) : 240,
-    preview = args['p'],
-    initialize = args['i'];
-
-exports.framerate = framerate;
-exports.args = args;
+const fs = require("mz/fs"),
+      args = require('minimist')(process.argv.slice(2)),
+      video = ( args['v'] && fs.existsSync(args['v']) ) ? args['v'] : false,
+      audio = ( args['a'] && fs.existsSync(args['a']) ) ? args['a'] : false,
+      framerate = args['r'] ? parseInt(args['r']) : 24,
+      size = args['s'] ? parseInt(args['s']) : 240,
+      preview = args['p'],
+      initialize = args['i'];
 
 // require some args
 if ( !video || !audio ) {
@@ -27,11 +20,21 @@ if (initialize && fs.existsSync(dir))
 if (!fs.existsSync(dir))
     fs.mkdirSync(dir);
 
-// load data
-exports.frameCount = function () { return require("./components/extract-frames").frames(video, size); }
+exports.framerate = framerate;
+exports.args = args;
+
+// data functions
+exports.frameCount = function () {
+    return require("./components/extract-frames").frames(video, size);
+}
 exports.diffs = function () { 
         const frameCount = require("./components/extract-frames").frames(video, size);
         return require('./components/compare-frames').diffs(frameCount);
 }
-exports.wave = function () { return require('./components/sample-audio').wave(audio, framerate); }
-exports.export = function (sequence) { require('./components/export-sequence').concat(sequence, size, framerate, audio, preview); }
+exports.wave = function () {
+    return require('./components/sample-audio').wave(audio, framerate);
+}
+
+exports.export = function (sequence) {
+    require('./components/export-sequence').concat(sequence, size, framerate, audio, preview);
+}
