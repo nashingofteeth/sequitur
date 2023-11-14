fa153beb1f67bc6ed2a837572a7a21ae701a83e2
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-11-13 17:15:19 -0800

    prefix frames folder with video name

--- src/components/extract-frames.js
+++ src/components/extract-frames.js
@@ -2,19 +2,19 @@ const fs = require("mz/fs"),
       path = require('path'),
       { execSync } = require("child_process");
 
-exports.frames = function(videoFile, resolution) {
-    let frameCount = countFrames();
+exports.frames = function(file, resolution) {
+    let frameCount = countFrames(file);
 
     if (!frameCount) {
-        extractFrames(videoFile, resolution);
-        frameCount = countFrames();
+        extractFrames(file, resolution);
+        frameCount = countFrames(file);
     }
 
     return frameCount;
 }
 
-function countFrames() {
-    const dir = 'data/frames/';
+function countFrames(file) {
+    const dir = 'data/frames_' + path.basename(file) + '/';
     if (fs.existsSync(dir)) {
         files = fs.readdirSync(dir);
         if (files.length < 2) return false;
@@ -27,10 +27,10 @@ function countFrames() {
 }
 
 function extractFrames(file, res) {
-    const dir = 'data/frames/';
+    const dir = 'data/frames_' + path.basename(file) + '/';
     if (!fs.existsSync(dir))
         fs.mkdirSync(dir)
 
     console.log('extracting frames...');
-    execSync("ffmpeg -i " + file.replace(' ','\\ ') + " -vf scale=-1:" + res + " data/frames/%d.bmp -y");
+    execSync('ffmpeg -i ' + file.replace(' ','\\ ') + ' -vf scale=-1:' + res + ' data/frames_' + path.basename(file) + '/%d.bmp -y');
 }
