f6c993f0d3e73ccf6c08ffde4d8ec39c65924977
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-11-15 11:29:41 -0800

    rewrite interposing sequencer

--- sequencers/interpose-sequencer.js
+++ sequencers/interpose-sequencer.js
@@ -1,100 +1,41 @@
-const fs = require("mz/fs");
-const { exec } = require("child_process");
+const seq = require('../src/sequitur'),
+      path = require('path');
 
-exec('mkdir data/frames1 data/frames2');
-var decode = "ffmpeg -i input/video1.mp4 -qscale:v 2 data/frames1/%d.bmp; ffmpeg -i input/video2.mp4 -qscale:v 2 data/frames2/%d.bmp"
+var videoFiles = seq.args['v'],
+    duration = 1/seq.framerate,
+    videos = {},
+    videoName = '',
+    frameCount = 0;
 
-console.log('DECODING...');
-exec(decode, (error, stdout, stderr) => {
-    // if (error) {
-    //     console.log(`error: ${error.message}`);
-    //     return;
-    // }
-    // if (stderr) {
-    //     console.log(`stderr: ${stderr}`);
-    //     return;
-    // }
-    // console.log(`stdout: ${stdout}`);
+for (v in videoFiles) {
+   videoName = path.basename(videoFiles[v]);
+   frameCount = seq.frameCount(videoFiles[v])
+   videos[v] = {};
+   videos[v].name = videoName;
+   videos[v].frames = frameCount;
+}
 
-    
-    fs.readdir('data/frames1/', (err, files) => {
-        videoALength = files.length;
+var longest = '0';
+for (v in videos)
+    if (videos[v].frames > videos[longest].frames)
+        longest = v;
 
-        fs.readdir('data/frames2/', (err, files) => {
-            videoBLength = files.length;
-
-            if (videoALength > videoBLength) sequence(videoBLength);
-            else sequence(videoALength);
-        });
-    });
-});
-
-
-var out = '', frameRate = 60;
-
-
-function sequence(f) {
-
-    for (l=1;l<f+1;l++) {
-
-// OUTPUT FRAME
-        var progress = l/f;
-
-        function isEven(n) {
-           return n % 2 == 0;
+sequence = '';
+for (i = 1; i <= videos[longest].frames; i++) {
+    for (v in videos) {
+        if (seq.args['stretch']) {
+            if ( videos[v] != videos[longest] )
+                frame = Math.ceil((i / videos[longest].frames) * videos[v].frames);
+            else frame = i;
+        }
+        else {
+            if ( i > videos[v].frames ) continue;
+            frame = i;
         }
 
-        if (isEven(l)) folder = 2;
-        else folder = 1;
-
-        console.clear();
-        console.log("SEQUENCING / " + (progress*100).toFixed(2) +"%");
-
-        // console.log(l + " / " + levels.length);
-
-        out += "file 'frames" + folder + "/" + l + ".bmp'\n" +
-                     "duration " + 1/frameRate + "\n";
-
-    }
-
-
-    fs.writeFile('data/seq.txt', out, function (err) {
-        if (err) throw err;
-
-        encode();
-    });
-
-    
+        sequence += "file 'frames_" + videos[v].name + "/" + frame + ".bmp'\n" +
+                    "duration " + duration + "\n";
+    } 
 }
 
-function encode(a) {
-    let current = new Date();
-    let cYear = current.getFullYear();
-    let cMonth = ('0'+(current.getMonth() + 1)).slice(-2);
-    let cDay = ('0'+current.getDate()).slice(-2);
-    let cHour = ('0'+current.getHours()).slice(-2);
-    let cMinute = ('0'+current.getMinutes()).slice(-2);
-    let cSecond = ('0'+current.getSeconds()).slice(-2);
-    let cDate = cYear + '' + cMonth + '' + cDay;
-    let cTime = cHour + '' + cMinute + '' + cSecond;
-    let dateTime = cDate + '' + cTime;
-
-    const encode = "ffmpeg -f concat -i data/seq.txt -i input/music.mp3 -vsync 1 -c:v prores_ks -profile:v 2 -c:a pcm_s16le -r "+frameRate+" exports/interposition_"+dateTime+".mov -y;";
-
-    console.log('ENCODING...');
-
-    exec(encode, (error, stdout, stderr) => {
-        // if (error) {
-        //     console.log(`error: ${error.message}`);
-        //     return;
-        // }
-        // if (stderr) {
-        //     console.log(`stderr: ${stderr}`);
-        //     return;
-        // }
-        // console.log(`stdout: ${stdout}`);
-
-        console.log("DONE");
-        exec("afplay /System/Library/PrivateFrameworks/ScreenReader.framework/Versions/A/Resources/Sounds/DrillOut.aiff");
-    });
-}
+seq.export(sequence);
