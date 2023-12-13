5a8112bd450c9146cd99b46c228cf8094ca5d98a
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-12-13 13:33:09 -0800

    place sorting function inside compare-frames

--- sequencers/diffing-wav-rider.js
+++ sequencers/diffing-wav-rider.js
@@ -2,16 +2,16 @@ const seq = require('../src/sequitur');
 
 (async() => {
 	const wave = seq.wave(),
-		  diffs = await seq.diffs();
-	
+		  diffs = await seq.diffs(undefined, undefined, true);
+
 	sequence(wave, diffs);
 })()
 
 function sequence(wave, diffs) {
 	var sequence = [],
 		frameCount = Object.keys(diffs).length,
-		frame = '1',
-		nextFrame = frame,
+		currentFrame = '1',
+		nextFrame = currentFrame,
 		occurance = [],
 		diffLimit = seq.args['dl'] || 1, // set between 0 and 1
 		marginThreshold = seq.args['mt'] || 0; // set between 0 and 1
@@ -26,17 +26,17 @@ function sequence(wave, diffs) {
 	// to do: add playhead option for sequencial frames below threshold
 	for (a in wave) {
 		let amplitude = parseFloat(wave[a]),
-			currentDiffsSorted = sortDiffs(diffs[frame]),
+			currentDiffs = diffs[currentFrame],
 			nextFrameIndex = Math.floor( (frameCount - 1) * (amplitude * diffLimit) ), // use amplitude as diffs index
-			nextFrame = currentDiffsSorted.array[String(nextFrameIndex)][0]
+			nextFrame = currentDiffs[String(nextFrameIndex)][0]
 			margin = 0;
-			
+
 		// amplitude threshold for frame reuse margin
 		if ( amplitude < marginThreshold )
 			margin = frameCount - 1;
 
 		// restrict frame reuse
-		let forward = true,
+		var forward = true,
 			distanceFoward = 0,
 			distanceBack = 0,
 			targetIndex = nextFrameIndex;
@@ -56,7 +56,7 @@ function sequence(wave, diffs) {
 
 			foward = forward ? false : true;
 
-			nextFrame = currentDiffsSorted.array[String(nextFrameIndex)][0];
+			nextFrame = currentDiffs[String(nextFrameIndex)][0];
 		}
 
 		// record occurence
@@ -65,26 +65,8 @@ function sequence(wave, diffs) {
 
 		sequence.push([nextFrame, 1/seq.framerate]);
 
-		frame = nextFrame;
+		currentFrame = nextFrame;
 	}
 
 	seq.export(sequence);
-}
-
-// to do: place inside compare-frames module
-function sortDiffs(o) {
-	var sorted = [];
-
-	for (d in o)
-		sorted.push([d, o[d]]);
-
-	sorted.sort(function(a, b) {
-		return a[1] - b[1];
-	});
-
-	return {
-		array: sorted,
-		closest: sorted[1][0],
-		furthest: sorted[sorted.length-1][0]
-	}
 }
