0d59a5e10ef5b6db03a4fe88ba56ec806a7d69b9
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-12-14 14:13:06 -0800

    add variation on weaver sequencer

---
+++ sequencers/spaced-by-length-weaver.js
@@ -0,0 +1,39 @@
+const seq = require('../src/sequitur'),
+      path = require('path');
+
+var videoFiles = seq.args['v'],
+    duration = 1/seq.framerate,
+    videos = {},
+    videoName = '',
+    frameCount = 0;
+
+for (v in videoFiles) {
+   videoName = path.basename(videoFiles[v]);
+   frameCount = seq.frameCount(videoFiles[v])
+   videos[v] = {};
+   videos[v].name = videoName;
+   videos[v].frames = frameCount;
+}
+
+var sequenceLength = 0;
+for (v in videos)
+    sequenceLength += videos[v].frames;
+
+var sequence = '',
+    sequenceArr = [];
+
+for (v in videos) {
+    for (i = 0; i < videos[v].frames; i++) {
+        let position = sequenceLength * (i / videos[v].frames);
+        sequenceArr.push([position, videos[v].name, i+1]);
+    }
+}
+
+sequenceArr.sort(function(a, b) {
+    return a[0] - b[0];
+});
+
+for (f in sequenceArr)
+    sequence += "file 'frames_" + sequenceArr[f][1] + "/" + sequenceArr[f][2] + ".bmp'\n" + "duration " + duration + "\n";
+
+seq.export(sequence);
