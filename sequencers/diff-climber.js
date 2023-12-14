0fe27b2d14ccbdb9f809b99aa5d48fd940b9eb25
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-12-13 16:58:50 -0800

    make ascending difference sequencer

---
+++ sequencers/diff-climber.js
@@ -0,0 +1,25 @@
+const seq = require('../src/sequitur');
+
+(async() => {
+	const diffs = await seq.diffs(undefined, undefined, true);
+
+	sequence(diffs);
+})()
+
+function sequence(diffs) {
+	var sequence = [],
+		frameCount = Object.keys(diffs).length,
+		previousFrame = '1',
+		sequenceLength = seq.args['length'] || frameCount,
+		distanceLimit = seq.args['limit'] || 1; // between 0 and 1
+
+	for (i = 0; i < sequenceLength; i++) {
+		let distance = Math.floor( frameCount * (i / sequenceLength) * distanceLimit );
+		console.log(distance);
+		nextFrame = diffs[previousFrame][distance][0];
+		sequence.push([nextFrame, 1/seq.framerate]);
+		previousFrame = nextFrame;
+	}
+
+	seq.export(sequence);
+}
