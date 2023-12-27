19f0202a10d5bc60fbbe6611f9ed945ec92e3b48
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-12-27 14:35:02 -0800

    remove contradictory fps_mode arg

--- src/components/export-sequence.js
+++ src/components/export-sequence.js
@@ -18,8 +18,8 @@ exports.concat = function(seq, res, fps, vid, aud, pre) {
     if (!fs.existsSync(dir))
         fs.mkdirSync(dir);
 
-    const preview = "ffmpeg -f concat -i data/seq.txt -vf scale=-1:" + res + " -vcodec libx264 -crf 30 -pix_fmt yuv420p -fps_mode vfr -r " + fps + " exports/sequitur_" + Date.now() + ".mp4",
-          full = "ffmpeg -f concat -i data/seq.txt -vf scale=-1:" + res + " -c:v prores_ks -profile:v 2 -c:a pcm_s16le -fps_mode vfr -r " + fps + " exports/sequitur_" + Date.now() + ".mov";
+    const preview = "ffmpeg -f concat -i data/seq.txt -vf scale=-1:" + res + " -vcodec libx264 -crf 30 -pix_fmt yuv420p -r " + fps + " exports/sequitur_" + Date.now() + ".mp4",
+          full = "ffmpeg -f concat -i data/seq.txt -vf scale=-1:" + res + " -c:v prores_ks -profile:v 2 -c:a pcm_s16le -r " + fps + " exports/sequitur_" + Date.now() + ".mov";
 
     var encodeCmd = pre ? preview : full;
 
