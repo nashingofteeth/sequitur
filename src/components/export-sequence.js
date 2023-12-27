ca0df53a195cbf0a725333145012ca4cc66930ab
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2023-12-27 15:31:49 -0800

    play video with ffplay after export

--- src/components/export-sequence.js
+++ src/components/export-sequence.js
@@ -3,26 +3,28 @@ const fs = require("mz/fs"),
       { execSync } = require("child_process");
       
 exports.concat = function(seq, res, fps, vid, aud) {
-    let seqStr = '';
+    let seqStr = '',
+        dir = 'exports',
+        filepath = `${dir}/sequitur_${ Date.now() }.mov`,
+        encodeCmd = `ffmpeg -f concat -i data/seq.txt -vf scale=-1:${res} -c:v prores_ks -profile:v 2 -c:a pcm_s16le -r ${fps} ${filepath}`;
+
+    if (aud) encodeCmd += ' -i ' + aud.replace(' ','\\ ');
+
     if (Array.isArray(seq)) {
         for (f in seq) {
-            seqStr += "file 'frames_" + path.basename(vid) + "/" + seq[f][0] + ".bmp'\n" +
-                      "duration " + seq[f][1] + "\n";
+            seqStr += `file 'frames_${path.basename(vid)}/${seq[f][0]}.bmp'\nduration ${seq[f][1]}\n`;
         }
     }
     else seqStr = seq;
-
     fs.writeFileSync('data/seq.txt', seqStr);
 
-    let dir = 'exports';
     if (!fs.existsSync(dir))
         fs.mkdirSync(dir);
-
-    const encodeCmd = "ffmpeg -f concat -i data/seq.txt -vf scale=-1:" + res + " -c:v prores_ks -profile:v 2 -c:a pcm_s16le -r " + fps + " exports/sequitur_" + Date.now() + ".mov";
-
-    if (aud) encodeCmd += ' -i ' + aud.replace(' ','\\ ');
 
     console.log('exporting...');
     execSync(encodeCmd);
     console.log('exported');
+
+    console.log('playing...');
+    execSync('ffplay ' + filepath);
 }
