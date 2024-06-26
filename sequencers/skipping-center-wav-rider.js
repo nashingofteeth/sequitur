ecef4dcff41f3aab3374efdfdc772a8a9b8ac0b9
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2024-06-25 21:14:17 -0700

    split skipping rider

---
+++ sequencers/skipping-center-wav-rider.js
@@ -0,0 +1,21 @@
+const seq = require("../src/sequitur");
+const frameCount = seq.frameCount();
+const wave = seq.wave();
+const frameDuration = 1 / seq.framerate;
+
+const sequence = [];
+let selectedFrame = Math.floor(frameCount * Math.random());
+let reverse = false;
+
+for (let i = 0; i < wave.length - 1; i++) {
+  const level = wave[i];
+
+  reverse = (selectedFrame > (frameCount - 1) - selectedFrame);
+
+  if (reverse) selectedFrame = selectedFrame - Math.round(selectedFrame * level);
+  else selectedFrame = selectedFrame + Math.round(((frameCount - 1) - selectedFrame) * level);
+
+  sequence.push([selectedFrame + 1, frameDuration]);
+}
+
+seq.export(sequence);
