afa31481d952b87ed5de179276b7533833ca98cc
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2025-04-07 20:56:58 -0700

    increase export quality

--- components/export-sequence.js
+++ components/export-sequence.js
@@ -3,7 +3,7 @@ const path = require("node:path");
 const { execSync } = require("node:child_process");
 
 exports.concat = (sequence, fps, videoFile, audioFile, outFileName, preview, noaudio) => {
-  let encodeCmd = `ffmpeg -f concat -i cache/seq.txt -c:v prores_ks -profile:v 3 -pix_fmt yuv422p10le -qscale:v 11 -vendor apl0 -r ${fps}`;
+  let encodeCmd = `ffmpeg -f concat -i cache/seq.txt -c:v prores_ks -profile:v 3 -pix_fmt yuv444p10le -qscale:v 8 -vendor apl0 -r ${fps}`;
 
   if (preview) encodeCmd += " -vf scale=-1:240";
 
