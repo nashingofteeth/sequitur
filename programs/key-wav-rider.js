fd153dc6949ae40eb810fe5c6270df7dab54eb36
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2025-04-21 19:58:11 -0700

    add threshold option to key-wav-rider

--- programs/key-wav-rider.js
+++ programs/key-wav-rider.js
@@ -3,13 +3,26 @@ const frameCount = seq.frameCount();
 const wave = seq.wave();
 const frameDuration = 1 / seq.framerate;
 
+const threshold = seq.args.threshold;
+
+// Define the range to map values to (after thresholding)
+const minOutput = threshold ?? 0.0;
+const maxOutput = 1.0;
+
 const sequence = [];
 
 for (let i = 0; i < wave.length - 1; i++) {
-	const level = Number.parseFloat(wave[i]);
-	const selectedFrame = Math.ceil((frameCount - 1) * level);
+  let level = Number.parseFloat(wave[i]);
 
-	sequence.push([selectedFrame + 1, frameDuration]);
+  if (level < threshold) {
+    level = 0;
+  }
+
+  const normalizedLevel = (level - threshold) / (1 - threshold) * (maxOutput - minOutput) + minOutput;
+
+  const selectedFrame = Math.ceil((frameCount - 1) * normalizedLevel);
+
+  sequence.push([selectedFrame + 1, frameDuration]);
 }
 
 seq.export(sequence);
